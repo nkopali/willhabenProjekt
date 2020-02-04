2eb9748754af6dfc5d58bb9c262ede129b7a1346
Luca Rodiga <l.rodiga@gmx.at>
2020-02-04 16:37:02 +0100

    showMsgs and sendMsg shoul be working

---
+++ server/inbox/index.js
@@ -0,0 +1,14 @@
+const Router = require('express').Router;
+const {showMsgs, sendMsg, deleteMsg} = require('./controller');
+const router = Router();
+const bodyParser = require('body-parser');
+const checkAuth = require('../checkAuth');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:false}));
+
+router.get('/:id', showMsgs);
+router.post('/', sendMsg);
+router.delete('/', deleteMsg);
+
+module.exports = router;
